d45803d8363fc261f2e9a6295844893f41f9f30a
Glitch (cs50x-in-node) <none>
2022-04-27 14:51:29 +0000

    Initial commit ✌️

---
+++ week-1/mario/mario-less/index.js
@@ -0,0 +1,34 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+/*
+===This does not do it recursively===
+for some reason it glitches out on second wrong attempt
+*/
+function ask(){
+  rl.question("Height:",(n)=>{
+    rl.close();
+    if(n[0]==="-"){ask();return}
+    let height = parseInt(n)
+    if(height>= 8 || height<=1 || typeof(height)!=="number"){ask();return};
+    generateStaircase(height);
+  })
+}
+function generateStaircase(h){
+  for(let i=0;i<h;i++){
+    let s="";
+    for(let j=8;j>=i;j--){
+      s+= " ";
+    }
+    for(let j=0;j<=i;j++){
+      s+= "#";
+    }
+    console.log(s);
+  }
+}
+ask();
+
+
+
