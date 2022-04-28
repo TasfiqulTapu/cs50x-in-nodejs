1a219a7d300fb0cd1f6c2545b7d8690a7c9b3d4b
Glitch (cs50x-in-node) <none>
2022-04-28 15:02:57 +0000

    Added fancy way to browse file in index.js

---
+++ index.js
@@ -0,0 +1,43 @@
+console.log("hello world");
+const fs = require("fs");
+const inquirer = require("inquirer");
+const { createSpinner } = require("nanospinner");
+const weeks = fs.readdirSync("./").filter((file) => file.startsWith("week-"));
+
+let week, spinner;
+async function getProject() {
+  await inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "week",
+        message: "which week do you want to view?",
+        choices: weeks,
+      },
+    ])
+    .then((answers) => {
+      week = answers["week"];
+    });
+
+  await inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "project",
+        message: "which project do you want to view?",
+        choices: fs.readdirSync(week),
+      },
+    ])
+
+    .then((answers) => {
+      spinner = createSpinner("Looking into it").start();
+      setTimeout(() => {
+        spinner.success({text: `please input: \n node ${week}/${answers["project"]}`, mark: ":)", color: "cyan"});
+       // spinner.stop()
+      }, 1000);
+    });
+}
+
+getProject();
+
+//console.log(weeks);
