5fd6b4f71a294160b328ab61f4dd01b4a44e9e0b
Glitch (cs50x-in-node) <none>
2022-05-04 05:21:02 +0000

    redid week1

--- week-1/02-mario-less/index.js
+++ week-1/02-mario-less/index.js
@@ -1,35 +1,23 @@
-const readline = require("readline");
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-module.exports = ask;
-  function ask() {
-    rl.question("Height:", (n) => {
-      // rl.close();
-      if (n[0] === "-") {
-        ask();
-        return;
-      }
-      let height = parseInt(n);
-      if (height >= 8 || height <= 1 || typeof height !== "number") {
-        ask();
-        return;
-      }
-      generateStaircase(height);
-    });
-  }
-  function generateStaircase(h) {
-    for (let i = 0; i < h; i++) {
-      let s = "";
-      for (let j = 8; j >= i; j--) {
-        s += " ";
-      }
-      for (let j = 0; j <= i; j++) {
-        s += "#";
-      }
-      console.log(s);
-    }
-  }
+const getInput = require("../../getInput.js");
+module.exports = main;
 
-  ask();
+async function main() {
+  let height;
+  do {
+    height = parseInt(await getInput("Height: "));
+  } while (!height || height >= 8 || height <= 1 || typeof height !== "number");
+  generateStaircase(height);
+  process.exit()
+}
+function generateStaircase(h) {
+  for (let i = 0; i < h; i++) {
+    let s = "";
+    for (let j = 8; j >= i; j--) {
+      s += " ";
+    }
+    for (let j = 0; j <= i; j++) {
+      s += "#";
+    }
+    console.log(s);
+  }
+}
--- week-1/03-cash/index.js
+++ week-1/03-cash/index.js
@@ -1,35 +1,28 @@
-const readline = require("readline");
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+const getInput = require("../../getInput.js");
 
-//not my proudest work, but gotta make do
-module.exports = getCents;
-getCents()
+module.exports = main;
+async function main() {
+  let cents = await getCents();
+  let coins = getCoinAmount(cents);
+  console.log(coins);
+  process.exit(0);
+}
 let cents;
-function getCents(){
-  rl.question("Change owed: ",(input)=>{
-     cents = parseFloat(input)
-     if(cents == 0) getCoinAmount(cents) // its very late at night I can't think of any other solution
-     if(!cents || cents<0 || Math.floor(cents)!==cents){
-       getCents();
-       return;
-       }
-    getCoinAmount(cents)
-     
-  })
+async function getCents() {
+  let c;
+  do {
+    c = parseFloat(await getInput("Change owed: "));
+    if (c == 0) return c;
+  } while (!c || c < 0 || Math.floor(c) !== c);
+  return c;
 }
-function getCoinAmount(nCent){
-  let total=0;
-  let coins=[25,10,5,1]
-  for(let i=0;i<coins.length;i++){
-    total+= Math.floor(nCent/coins[i])
-    nCent -= Math.floor(nCent / coins[i])*coins[i]
+
+function getCoinAmount(nCent) {
+  let total = 0;
+  let coins = [25, 10, 5, 1];
+  for (let i = 0; i < coins.length; i++) {
+    total += Math.floor(nCent / coins[i]);
+    nCent -= Math.floor(nCent / coins[i]) * coins[i];
   }
-  console.log(total)
-  process.exit(0) //readline doesn't kill the process automatically
+  return total
 }
-
-
-
