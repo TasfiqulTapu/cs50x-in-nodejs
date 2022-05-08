1435cb8c55e2f453b1cadb192346680cb0b629fb
Glitch (cs50x-in-node) <none>
2022-05-08 11:26:04 +0000

    🏃‍♀️ Completed Runoff

--- week-3/02-runoff/index.js
+++ week-3/02-runoff/index.js
@@ -36,6 +36,9 @@ async function main() {
     process.exit();
   }
   for (let i = 0; i < voterCount; i++) {
+    preferences.push([]);
+  }
+  for (let i = 0; i < voterCount; i++) {
     for (let j = 0; j < candidateCount; j++) {
       let name = await getInput(`Rank ${j + 1}`);
       if (!vote(i, j, name)) {
@@ -74,25 +77,70 @@ async function main() {
 }
 
 function vote(i, j, name) {
-  //return false if name doesn't exist 
-  //add the candidate to preferences array 
-  // where i = voter index and j= preference index 
-  
+  //return false if name doesn't exist
+  //add the candidate to preferences array
+  // where i = voter index and j= preference index
+  for (let k = 0; k < candidates.length; k++) {
+    if (candidates[k].name == name) {
+      preferences[i].push(name);
+      return true;
+    }
+  }
+  return false;
 }
 function tabulate() {
   // add number of votes to each candidate ( who haven't been eliminated)
+
+  for (let i = 0; i < preferences.length; i++) {
+    for (let j = 0; j < preferences[i].length; j++) {
+      let desired = candidates.findIndex((e) => e.name == preferences[i][j]);
+      if (!desired.eliminated) candidates[desired].votes += 1;
+    }
+  }
 }
 function print_winner() {
-  // print name of candidate who has >50% votes and return true 
+  // print name of candidate who has >50% votes and return true
   //else return false
+  let x = [];
+  for (let i = 0; i < candidates.length; i++) {
+    if (!candidates[i].eliminated) x.push(candidates[i]);
+    if (candidates[i].votes >= voterCount / 2) {
+      console.log(candidates[i].name);
+      return true;
+    }
+  }
+  if ((x.length = 1)) {
+    console.log(x[0].name);
+    return true;
+  }
+  return false;
 }
 function find_min() {
   // return the minimum number of vote anyone in the election has
+  let min = voterCount;
+  for (let i = 0; i < candidates.length; i++) {
+    if (candidates[i].votes < min && candidates[i].eliminated == false) {
+      min = candidates[i].votes;
+    }
+  }
+  return min;
 }
 function is_tie(min) {
-  // return true if everyone left has min votes 
+  // return true if everyone left has min votes
   // else false
+  let notLowest;
+  for (let i = 0; i < candidates.length; i++) {
+    if (candidates[i].votes > min && candidates[i].eliminated == false) {
+      notLowest == candidates[i];
+    }
+  }
+  return !!notLowest;
 }
 function eliminate(min) {
-  // eliminate anyone who has min votes 
+  // eliminate anyone who has min votes
+  for (let i = 0; i < candidates.length; i++) {
+    if (candidates[i].votes <= min && candidates[i].eliminated == false) {
+      candidates[i].eliminated = true;
+    }
+  }
 }
